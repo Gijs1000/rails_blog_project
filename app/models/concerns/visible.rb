9e8bcb9dceae7d4e95a42c3ad1bbb10ff5a23b20
Gijs Bolhuis <bolhuisgijs@gmail.com>
2021-06-14 14:57:54 +0200

    Add basic security features.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -4,7 +4,7 @@ module Visible
   VALID_STATUSES = ['public', 'private', 'archived']
 
   included do
-    validates :status, inclusion { in: VALID_STATUSES }
+    validates :status, inclusion: { in: VALID_STATUSES }
   end
 
   class_methods do
@@ -12,7 +12,8 @@ module Visible
       where(status: 'public').count
     end
 
-  def archived?
-    status == 'archived'
+    def archived?
+      status == 'archived'
+    end
   end
 end
